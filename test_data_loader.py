b945158456a57d65631d38f0b83ee083ec5dc721
tmarv <timothy.marvel@protonmail.com>
2022-01-04 22:05:00 +0100

    dataloader and training works

--- test_data_loader.py
+++ test_data_loader.py
@@ -1,8 +1,14 @@
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 
+import matplotlib.pyplot as plt
+
 import src.tools
-from src import custom_data_loader, tools
+from src import custom_data_loader, tools, neural_net_lib
+
+import torch.optim as optim
+import torch.nn as nn
 
 # how to use batch size?
 '''
@@ -16,23 +22,38 @@ print(ttensor)
 '''
 pos_location, neg_location = tools.get_save_location_three()
 src.tools.get_save_location_three()
-params = {'batch_size': 128,
+params = {'batch_size': 32,
           'shuffle': True,
           'num_workers': 0}
 custom_set = custom_data_loader.CustomDataset(src.tools.get_save_location_three()[0],
                                               src.tools.get_save_location_three()[1])
 train_loader = DataLoader(custom_set, **params)
 
-print("started ")
-for i, data in enumerate(train_loader, 0):
-    # print("------ " +str(i)+" fff")
-    # print(len(data))
-    inputs, rewards = data
-    # make sure it is the same length as batch size
-    print(len(inputs))
-    # print(inputs)
-    # print(rewards)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+neural_net = neural_net_lib.ThreeByThreeSig().to(device)
+optimizer = optim.Adam(neural_net.parameters(), lr=0.0001)
+l1_loss = nn.SmoothL1Loss()
 
+
+train_losses = []
+print("started ")
+for e in range(2):
+    for i, data in enumerate(train_loader):
+        inputs, rewards = data
+        # make sure it is the same length as batch size
+        input_len = len(inputs)
+        inputs = inputs.reshape([input_len*4, 3, 3])
+        rewards = rewards.reshape([input_len*4, 1])
+        result = neural_net.forward(inputs)
+        train_loss = l1_loss(result, rewards)
+        optimizer.zero_grad()
+        train_loss.backward()
+        optimizer.step()
+        train_losses.append(train_loss)
+
+train_losses = np.array(train_losses)
+plt.plot(train_losses)
+plt.show()
 #exit()
 
 
